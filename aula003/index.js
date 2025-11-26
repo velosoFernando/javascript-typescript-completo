84b04e0745cdf1238713c77003471b0229419afc
velosoFernando <fernandovelosomkt@gmail.com>
2025-11-26 16:56:57 -0300

    studyON: Iniciando os estudos do curso de JS e TS completo

---
+++ aula003/index.js
@@ -0,0 +1,7 @@
+console.log('Fernando "Veloso"');
+console.log("'Fernando' Veloso");
+console.log(`'Fernando' "Veloso"`);
+
+console.log(123456);
+console.log(123456, 15645512.855454121);
+console.log(35, 15.85, "Fernando Veloso");
