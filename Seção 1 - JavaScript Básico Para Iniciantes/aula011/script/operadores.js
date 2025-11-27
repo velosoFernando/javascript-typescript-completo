802f6ae6cdde2f5ccad93c53c3dad38c306a69e7
velosoFernando <fernandovelosomkt@gmail.com>
2025-11-26 22:59:18 -0300

    adjust: Aula operadores!

--- Seção 1 - JavaScript Básico Para Iniciantes/aula011/script/operadores.js
+++ Seção 1 - JavaScript Básico Para Iniciantes/aula011/script/operadores.js
@@ -1,52 +1,52 @@
-let num1 = 5;
-let num2 = 10;
-console.log(num1 + num2);
+// let num1 = 5;
+// let num2 = 10;
+// console.log(num1 + num2);
 
-const num1 = '5';
-const num2 = 10;
-console.log(num1 + num2);
+// const num1 = '5';
+// const num2 = 10;
+// console.log(num1 + num2);
 
-let num1 = 5;
-let num2 = 10;
-console.log(num1 / num2);
+// let num1 = 5;
+// let num2 = 10;
+// console.log(num1 / num2);
 
-const num1 = 5;
-const num2 = 10;
-console.log(num1 * num2);
+// const num1 = 5;
+// const num2 = 10;
+// console.log(num1 * num2);
 
-const num1 = 5;
-const num2 = 10;
-console.log(num1 ** num2);
+// const num1 = 5;
+// const num2 = 10;
+// console.log(num1 ** num2);
 
-const num1 = 10;
-const num2 = 3;
-console.log(num1 % num2);
+// const num1 = 10;
+// const num2 = 3;
+// console.log(num1 % num2);
 
-const num1 = 5;
-const num2 = 2;
-const num3 = 10;
-console.log((num1 + num2) * num3);
+// const num1 = 5;
+// const num2 = 2;
+// const num3 = 10;
+// console.log((num1 + num2) * num3);
 
 
-// Operador de incremento
-// Só funciona com let
-// ++contador (pré) primeiro faz a conta e depois retorna o valor
-// contador++ (pós) primeiro executa a ação e depois adiciona o valor
+// // Operador de incremento
+// // Só funciona com let
+// // ++contador (pré) primeiro faz a conta e depois retorna o valor
+// // contador++ (pós) primeiro executa a ação e depois adiciona o valor
 
-let contador = 1;
-contador++; // 2
-contador++; // 3
-contador++; // 4
-contador++; // 5
-++contador; // 6
-++contador; // 7
-++contador; // 8
-++contador; // 9
-++contador; // 10
-console.log(contador);
+// let contador = 1;
+// contador++; // 2
+// contador++; // 3
+// contador++; // 4
+// contador++; // 5
+// ++contador; // 6
+// ++contador; // 7
+// ++contador; // 8
+// ++contador; // 9
+// ++contador; // 10
+// console.log(contador);
 
-// Operador de decremento
-// Só funciona com let
-// --contador(pré)
-// contador-- (pós)
+// // Operador de decremento
+// // Só funciona com let
+// // --contador(pré)
+// // contador-- (pós)
 
